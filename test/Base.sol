82f51587ebab01c7f0257a2fe6d00767e7839019
Clément <clemmoller@gmail.com>
2025-06-09 18:17:17 +0200

    :sparkles: Add Base and Setup for invariant testing.

---
+++ test/Base.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+// Foundry
+import {Test} from "forge-std/Test.sol";
+
+// Maverick
+import {MaverickV2Pool} from "@rooster-pool/v2-amm/contracts/MaverickV2Pool.sol";
+import {MaverickV2Factory} from "@rooster-pool/v2-amm/contracts/MaverickV2Factory.sol";
+import {MaverickV2Position} from "@rooster-pool/v2-supplemental/contracts/MaverickV2Position.sol";
+import {MaverickV2LiquidityManager} from "@rooster-pool/v2-supplemental/contracts/MaverickV2LiquidityManager.sol";
+
+// ERC
+import {ERC20} from "@solmate/tokens/ERC20.sol";
+
+abstract contract Base_Test is Test {
+    //////////////////////////////////////////////////////
+    /// --- CONTRACTS
+    //////////////////////////////////////////////////////
+    ERC20 public weth;
+    ERC20 public oeth;
+    MaverickV2Pool public pool;
+    MaverickV2Factory public factory;
+    MaverickV2Position public position;
+    MaverickV2LiquidityManager public liquidityManager;
+
+    address public boostedPositionFactory;
+
+    //////////////////////////////////////////////////////
+    /// --- Governance, multisigs and EOAs
+    //////////////////////////////////////////////////////
+    address[] public users;
+
+    address public alice;
+    address public bobby;
+    address public clark;
+    address public david;
+    address public elsie;
+    address public glenn;
+    address public henry;
+    address public irene;
+    address public jenny;
+    address public kevin;
+
+    address public deployer;
+    address public governor;
+    address public operator;
+}
---
+++ test/Setup.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+// Test imports
+import {Base_Test} from "./Base.sol";
+
+// Maverick contracts
+import {MaverickV2Pool} from "@rooster-pool/v2-amm/contracts/MaverickV2Pool.sol";
+import {MaverickV2Factory} from "@rooster-pool/v2-amm/contracts/MaverickV2Factory.sol";
+import {MaverickV2Position} from "@rooster-pool/v2-supplemental/contracts/MaverickV2Position.sol";
+import {MaverickV2LiquidityManager} from "@rooster-pool/v2-supplemental/contracts/MaverickV2LiquidityManager.sol";
+
+// Maverick interfaces
+import {IMaverickV2Factory} from "@rooster-pool/v2-common/contracts/interfaces/IMaverickV2Factory.sol";
+import {IMaverickV2BoostedPositionFactory} from
+    "@rooster-pool/v2-supplemental/contracts/interfaces/IMaverickV2BoostedPositionFactory.sol";
+
+// Mocks
+import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
+import {IWETH9} from "@rooster-pool/v2-supplemental/contracts/paymentbase/IWETH9.sol";
+import {MockERC20} from "@solmate/test/utils/mocks/MockERC20.sol";
+
+contract Setup is Base_Test {
+    //////////////////////////////////////////////////////
+    /// --- SETUP
+    //////////////////////////////////////////////////////
+    function setUp() public virtual {
+        // 1. Setup a realistic test environnement.
+        _setUpRealisticEnvironnement();
+
+        // 2. Create user.
+        _createUsers();
+
+        // 3. Deploy external contracts.
+        _deployExternal();
+
+        // 4. Deploy contracts.
+        //_deployContracts();
+
+        // 5. Initialize users and contracts.
+        //_initiliaze();
+    }
+
+    //////////////////////////////////////////////////////
+    /// --- ENVIRONMENT
+    //////////////////////////////////////////////////////
+    function _setUpRealisticEnvironnement() private {
+        vm.warp(1750000000);
+        vm.roll(23000000);
+    }
+
+    //////////////////////////////////////////////////////
+    /// --- USERS
+    //////////////////////////////////////////////////////
+    function _createUsers() private {
+        // Random users
+        users.push(alice = makeAddr("Alice"));
+        users.push(bobby = makeAddr("Bobby"));
+        users.push(clark = makeAddr("Clark"));
+        users.push(david = makeAddr("David"));
+        users.push(elsie = makeAddr("Elsie"));
+        users.push(glenn = makeAddr("Glenn"));
+        users.push(henry = makeAddr("Henry"));
+        users.push(irene = makeAddr("Irene"));
+        users.push(jenny = makeAddr("Jenny"));
+        users.push(kevin = makeAddr("Kevin"));
+
+        // Permissionned users
+        deployer = makeAddr("Deployer");
+        governor = makeAddr("Governor");
+        operator = makeAddr("Operator");
+
+        // Mocked addresses
+        vm.label(boostedPositionFactory, "Boosted Position Factory");
+    }
+
+    //////////////////////////////////////////////////////
+    /// --- EXTERNAL CONTRACTS
+    //////////////////////////////////////////////////////
+    function _deployExternal() private {
+        // Deploy OETH
+        oeth = new MockERC20("Optimism Ether", "OETH", 18);
+        // Deploy WETH
+        weth = new MockERC20("Wrapped Ether", "WETH", 18);
+
+        // Deploy Maverick V2 Factory
+        factory = new MaverickV2Factory(governor);
+
+        // Deploy Maverick V2 Position
+        position = new MaverickV2Position(IMaverickV2Factory(address(factory)));
+
+        // Deploy Maverick V2 Liquidity Manager
+        liquidityManager = new MaverickV2LiquidityManager(
+            IMaverickV2Factory(address(factory)),
+            IWETH9(address(weth)),
+            position,
+            IMaverickV2BoostedPositionFactory(boostedPositionFactory)
+        );
+
+        // Deploy WETH/OETH Maverick V2 Pool
+        pool = MaverickV2Pool(
+            address(factory.create(100000000000000, 1, 300, IERC20(address(weth)), IERC20(address(oeth)), -1, 1))
+        );
+
+        // Label all freshly deployed external contracts
+        vm.label(address(oeth), "OETH");
+        vm.label(address(weth), "WETH");
+        vm.label(address(factory), "Maverick V2 Factory");
+        vm.label(address(position), "Maverick V2 Position");
+        vm.label(address(liquidityManager), "Maverick V2 Liquidity Manager");
+        vm.label(address(pool), "WETH/OETH Maverick V2 Pool");
+    }
+
+    function test() public {}
+}
